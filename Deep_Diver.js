8469f65f09eefa14600e30e6c2f0645a180a64ae
JohnBCasey <caseyjohnb@gmail.com>
2016-10-30 14:04:27 -0400

    Initial Commit, header comment

---
+++ Deep_Diver.js
@@ -0,0 +1,10 @@
+/**
+*Title: Deep_Diver.js
+*Author: John B. Casey <github: caseyj> <email:caseyjohnb@gmail.com>
+*Language: Javascript
+*Description : A tool developed to automate deep copy operations of a large 
+		object network. With this system any javascript object should be taken 
+		and all connected pieces of data will be appropriately deep copied into
+		the target clone copy. 
+*
+*/
