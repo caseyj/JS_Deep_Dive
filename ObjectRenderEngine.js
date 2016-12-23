7a4733b93eee9bb831e6c0dbd5d25c69a881f87e
JohnBCasey <caseyjohnb@gmail.com>
2016-12-23 00:27:52 -0500

    initial commit

---
+++ ObjectRenderEngine.js
@@ -0,0 +1,10 @@
+/*
+*Title: ObjectRenderEngine.js
+*Author: John B. Casey <caseyjohnb@gmail.com> <github: caseyj
+*Language: Javascript, Angular JS 
+*Description: a graphic web interfact to depict and describe the connections 
+*		between objects provided
+*/
+
+
+
