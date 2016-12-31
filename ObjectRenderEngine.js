a6866d4a0ae7f40c969e1361b5b89f79908144c4
JohnBCasey <caseyjohnb@gmail.com>
2016-12-31 00:35:23 -0500

    lines and circles drawn, next step will be arrows to show directionality and what the values are

--- ObjectRenderEngine.js
+++ ObjectRenderEngine.js
@@ -24,13 +24,10 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min+1)) + min;
 }
 
-var createPoint_fromObj = function(collector, objIndex){
-		collector[objIndex] = createPoint(getRandomInt(0,600), getRandomInt(0,600));
-}
 
 var generatePoints = function(collector, objectList){
 	Object.keys(objectList).forEach(function(key){
-		collector[key] = new createPoint(getRandomInt(0,600), getRandomInt(0,600));
+		collector[key] = new createPoint(getRandomInt(0,600), getRandomInt(0,300));
 	});
 	return collector
 }
@@ -44,10 +41,11 @@ var run_canvas = function($scope){
 
 	//define some functions which can be used to generate 2d data points and some lines
 
+	
 	var drawDot = function(context, p){
 		if(p!=null){
 			context.beginPath();
-			context.arc(p.x,p.y,10, 2*3.14, false);
+			context.arc(p.x,p.y,10, 0, 2*3.14, false);
 			context.fillStyle = "#000000";
 			context.fill();
 			context.lineWidth = 1;
@@ -58,6 +56,8 @@ var run_canvas = function($scope){
 	
 	var drawLine = function(context, p1, p2){
 		if(p1!=null && p2!=null){
+			console.log(p1);
+			console.log(p2);
 			context.beginPath();
 			context.moveTo(p1.x, p1.y);
 			context.lineTo(p2.x, p2.y);
@@ -71,13 +71,16 @@ var run_canvas = function($scope){
 		var edges = decomposed_network["edge_list"];
 		var ID_to_point = generatePoints({}, node_list);
 		console.log(ID_to_point);
-		for(var i = 0; i<edges.length; i++){
+		Object.keys(ID_to_point).forEach(function(key){
+			drawDot(context, ID_to_point[key]);
+		});
+		for(var i = 1; i<edges.length; i++){
 			var edge = edges[i];
 			switch(edges[i]["data_type"]){
 				case "object":
 				case "array":
-					drawDot(context, ID_to_point[edge["data"]])
-					drawLine(context, ID_to_point[edge["data"]], ID_to_point[edge["my_ID"]])
+					console.log(edge);
+					drawLine(context, ID_to_point[edge["data"]], ID_to_point[edge["source_identifier"]])
 					break;
 				default:
 					break;
@@ -88,7 +91,7 @@ var run_canvas = function($scope){
 	//iteration function
 	//execute those functions on our "root"
 	canvas.width = 600;
-	canvas.height = 600;
+	canvas.height = 300;
 	//context.globalAlpha = 1.0;
 	//var LL = classic_LL(2);
 	var o1 = {};
@@ -108,8 +111,18 @@ var run_canvas = function($scope){
 	o2["data"] = 2;
 	o2["next"] = null;
 	var network = decompose_network(o1);
-	context.beginPath();
 	drawLines(network, context);
-
-
+	/*
+	canvas.width = 600;
+	canvas.height = 300;
+	context.beginPath();
+	context.arc(100,100,10,0, 2*3.14, false);
+	context.fillStyle = "#000000";
+	context.stroke();
+	context.beginPath();
+			context.moveTo(100, 100);
+			context.lineTo(50, 50);
+			context.strokeStyle = "black";
+			context.stroke();
+			*/
 }
